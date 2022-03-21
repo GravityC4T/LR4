9f72942aa207e9ef2372ae245da9e98f3f4108e7
GravityC4T <GravityC4T>
2022-03-21 22:58:52 +0200

    Commit 1

--- 1.py
+++
@@ -1,6 +0,0 @@
-name = input("What's your name? ")
-old = input("How old are you? ")
-address = input("Where do you live? ")
-print("This is", name,",")
-print("Who is", old,"years old")
-print("And lives in", address)
